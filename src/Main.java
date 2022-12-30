946ddc58a52370d70a0593375457c28b8c4b58d5
Ivan <i.klimenko.eng@gmail.com>
2022-12-30 21:27:33 +0400

    Task calc started

--- src/Main.java
+++ src/Main.java
@@ -12,31 +12,31 @@ public class Main {
 //    }
 
     public static void main(String[] args) {
+        System.out.println("Программа \"Калькулятор\".");
+        System.out.println("Калькулятор производит операции с целыми римскими и арабскими числами от 1 до 10.");
+        System.out.println("Возможные операции: сложение(+), вычитание(-), умножение(*) и деление(/).");
         System.out.print("Введите выражение через пробел: ");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        String[] inputStr = input.split(" ");
+
+
+        for (int i = 0; i < inputStr.length; i++) {
+            if (inputStr[i].equals("+")) {
+                int res = Short.parseShort(inputStr[i - 1]) + Short.parseShort(inputStr[i + 1]);
+                System.out.println(res);
+            } else if (inputStr[i].equals("-")) {
+                int res = Short.parseShort(inputStr[i - 1]) - Short.parseShort(inputStr[i + 1]);
+                System.out.println(res);
+            } else if (inputStr[i].equals("*")) {
+                int res = Short.parseShort(inputStr[i - 1]) * Short.parseShort(inputStr[i + 1]);
+                System.out.println(res);
+            } else if (inputStr[i].equals("/")) {
+                int res = Short.parseShort(inputStr[i - 1]) / Short.parseShort(inputStr[i + 1]);
+                System.out.println(res);
+
+
 //        System.out.println(calc(input))
-        String[] str = input.split(" ");
-        for (int i = 0; i < str.length; i++) {
-
-        }
-
-
-
-        for (int i = 0; i < str.length; i++) {
-            if (str[i].equals("+")) {
-                int res = Short.parseShort(str[i - 1]) + Short.parseShort(str[i + 1]);
-                System.out.println(res);
-            } else if (str[i].equals("-")) {
-                int res = Short.parseShort(str[i - 1]) - Short.parseShort(str[i + 1]);
-                System.out.println(res);
-            } else if (str[i].equals("*")) {
-                int res = Short.parseShort(str[i - 1]) * Short.parseShort(str[i + 1]);
-                System.out.println(res);
-            } else if (str[i].equals("/")) {
-                int res = Short.parseShort(str[i - 1]) / Short.parseShort(str[i + 1]);
-                System.out.println(res);
-
             }
         }
 
